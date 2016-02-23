c86d5eebc073d605536c0570fb96226ed7306b60
gnios <eugenio00@gmail.com>
2016-02-23 08:43:49 -0300

    Ids não podem ser repetidos....

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,33 @@
 function produto(id, nome, valorUnitario) {
-    this.id = id;
-    this.nome = nome + Date.now();
+    this.id = id + "PRODUTO";
+    this.nomeProduto = nome;
     this.valorUnitario = valorUnitario;
-    this._lastChange = Date.now();
+    this._lastChange = new Date("October 13, 2014 11:13:00");
+    this._removed = false; // Quando true, esse registro será removido do app.
+}
+
+function cliente(id, nome, empresa) {
+    this.id = id + "CLIENTE";
+    this.nomeCliente = nome;
+    this.empresa = empresa;
+    this._lastChange = new Date("October 13, 2014 11:13:00");
     this._removed = false; // Quando true, esse registro será removido do app.
 }
 
 function updatesMock() {
-    this.now = Date.now();
+    this.now = new Date("October 13, 2014 11:13:00");
     this.updates = [];
-    for (var index = 1; index < 1001; index++) {
+    for (var index = 1; index < 100; index++) {
         this.updates.push(new produto(index, "produto" + index, index * 100));
+    }
+    return this;
+}
+
+function mockClientes() {
+    this.now = new Date("October 13, 2014 11:13:00");
+    this.updates = [];
+    for (var index = 1; index < 100; index++) {
+        this.updates.push(new cliente(index, "nome" + index, "empresa" + index));
     }
     return this;
 }
@@ -28,6 +45,15 @@ var appRouter = function (app) {
         res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
         console.log(req.query.since);
         var updatesObjects = new updatesMock();
+        res.send(JSON.stringify(updatesObjects));
+    });
+
+    app.get("/clientes/", function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+        console.log(req.query.since);
+        var updatesObjects = new mockClientes();
         res.send(JSON.stringify(updatesObjects));
     });
 
